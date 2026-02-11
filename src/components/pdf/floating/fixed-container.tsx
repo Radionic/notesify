985528295a135668971b43f0a2be4723dd7e45fc
Radionic <khpluslt@gmail.com>
2026-02-12 01:24:03 +0800

    chore: remove unused files

--- src/components/pdf/floating/fixed-container.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-export const FixedContainer = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="absolute top-0 left-0 w-full h-full pointer-events-none">
-      {children}
-    </div>
-  );
-};
